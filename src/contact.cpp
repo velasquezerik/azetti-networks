7d469cd0ec2407a7dc645f84a66a2862806dd814
Erik Velasquez <erikvelasquez.25@gmail.com>
2018-08-14 16:54:25 -0400

    some changes in cmake

---
+++ src/contact.cpp
@@ -0,0 +1,29 @@
+/*******************************************************************************
+* Contact - functions definitions
+*
+* Declaration and definition of functions needed by the Contact model
+*
+* Author: Erik Velasquez
+* Date:   2018-08-14
+*
+* Revision History:
+*   2018-Aug-14: Erik Velasquez
+*                  - Functions creation
+*******************************************************************************/
+
+
+/* >>>>>>>>>>>>>>>>>>>>>>>> Standard C/C++ Libraries >>>>>>>>>>>>>>>>>>>>>>>> */
+/* EMPTY */
+
+/* >>>>>>>>>>>>>>>>>>>>>>>>>>>>>> POSIX Headers >>>>>>>>>>>>>>>>>>>>>>>>>>>>> */
+/* EMPTY */
+
+/* >>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Local Headers >>>>>>>>>>>>>>>>>>>>>>>>>>>>> */
+
+#include "contact.h"
+
+/***** Functions Definitions **************************************************/
+
+/* EMPTY */
+
+/***** End Functions Definitions **********************************************/
---
+++ src/user.cpp
@@ -0,0 +1,29 @@
+/*******************************************************************************
+* User - functions definitions
+*
+* Declaration and definition of functions needed by the User model
+*
+* Author: Erik Velasquez
+* Date:   2018-08-14
+*
+* Revision History:
+*   2018-Aug-14: Erik Velasquez
+*                  - Functions creation
+*******************************************************************************/
+
+
+/* >>>>>>>>>>>>>>>>>>>>>>>> Standard C/C++ Libraries >>>>>>>>>>>>>>>>>>>>>>>> */
+/* EMPTY */
+
+/* >>>>>>>>>>>>>>>>>>>>>>>>>>>>>> POSIX Headers >>>>>>>>>>>>>>>>>>>>>>>>>>>>> */
+/* EMPTY */
+
+/* >>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Local Headers >>>>>>>>>>>>>>>>>>>>>>>>>>>>> */
+
+#include "user.h"
+
+/***** Functions Definitions **************************************************/
+
+/* EMPTY */
+
+/***** End Functions Definitions **********************************************/
